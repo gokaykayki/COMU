814c45063752dc3023766e1debf99a78a6f5993e
Gokay Kayki <gokaykayki@gmail.com>
2017-12-23 11:32:45 +0200

    rotate(matris ile) eklendi

---
+++ 7. Term/GoruntuIsleme/final/rotateWithMatrix.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[32]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+test = plt.imread("test.jpg")
+
+plt.imshow(test)
+plt.show()
+
+matrix = [[0,-1],[1,0]]
+
+bw = np.zeros((test.shape[0]*2, test.shape[1]*2), dtype=np.uint8)
+
+threshold = 120
+for i in range(0,100):
+    for j in range(0,100):
+        n = test[i,j,0]/3 + test[i,j,1]/3 + test[i,j,2]/3
+        if n > threshold:
+            bw[i+100,j+100] = 0
+        else:
+            bw[i+100,j+100] = 255
+
+
+
+for i in range(100,200):
+    for j in range(100,200):
+        newCoords = np.dot(matrix,[i,j])
+        print (newCoords)
+        bw[newCoords[0],newCoords[1]-100] = bw[i,j]
+
+plt.imshow(bw, plt.cm.binary)
+plt.show()
+
+
+# In[ ]:
+
+
+
